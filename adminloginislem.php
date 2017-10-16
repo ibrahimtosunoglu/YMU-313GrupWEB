6438ea5b38d90c8f3b34d631007fdf21fef4d4cb
ibrahimtosunoglu <ibrahimtosunoglu@users.noreply.github.com>
2017-10-16 11:17:59 +0300

    Admin Giriş Ekranı Yapıldı

---
+++ adminloginislem.php
@@ -0,0 +1,24 @@
+<?php
+$db = new PDO("mysql:host=localhost;dbname=web3;charset=utf8", "root", "");
+
+
+if($_POST)
+{
+    $nick =$_POST["nick"];
+    $sifre =$_POST["sifre"];
+    $query  = $db->query("SELECT * FROM admins WHERE nick='$nick' && sifre='$sifre'",PDO::FETCH_ASSOC);
+    if ( $say = $query -> rowCount() ){
+        if( $say > 0 ){
+            session_start();
+            $_SESSION['oturum']=true;
+            $_SESSION['name']=$nick;
+            $_SESSION['pass']=$sifre;
+
+            echo "oturum başarıyla açıldı";
+
+        }else{
+            echo "oturum açılmadı hata";
+        }
+    }
+}
+        ?>
